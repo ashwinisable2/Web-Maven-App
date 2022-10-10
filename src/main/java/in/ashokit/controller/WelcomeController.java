8eac22eec9ca629693fbea775ef00bf90c011e3e
Ashwini <ashwinikanawade91@gmail.com>
2022-10-10 10:52:08 +0530

    my first commit

---
+++ src/main/java/in/ashokit/controller/WelcomeController.java
@@ -0,0 +1,17 @@
+package in.ashokit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+	@GetMapping("/welcome")
+	public String welcome(Model model)
+	{
+		model.addAttribute("msg", "Welcome to Spring Boot..!!");
+		return "index";
+	}
+	
+}
